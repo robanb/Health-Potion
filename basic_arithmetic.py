4da1d8fe8760754025eadc785bb275f7f87cad29
Roba Nath Basnet <robanbth@gmail.com>
2025-03-25 23:25:06 +0600

    correction

--- basic_arithmetic.py
+++ basic_arithmetic.py
@@ -1,41 +1,9 @@
-Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 2 + 3
-5
->>> 2+3
-5
->>> 100-20
-80
->>> 100-20+10
-90
->>> 2*3
-6
->>> 2**3
-8
->>> 8/2
-4.0
->>> 8 / 2
-4.0
->>> 10 / 3
-3.3333333333333335
->>> 1/2
-0.5
->>> type(2)
-<class 'int'>
->>> type(0.2)
-<class 'float'>
->>> type(2.0)
-<class 'float'>
->>>  10 % 2
-...  
-SyntaxError: unexpected indent
->>> 10%2
-0
->>> 5 % 2
-1
->>> 10 % 2
-0
->>> 9%2
-1
->>> 7.5 % 5
-2.5
+print("addition (2 + 3 ) =" , 2 + 3)
+print("subtraction (2 - 3 ) =" , 2 - 3)
+print("multiplication (2 * 3 ) =" , 2 * 3)
+print("division (2 / 3 ) =" , 2 / 3)
+print("floor division (2 // 3 ) =" , 2 // 3)
+print("exponent (2 ** 3 ) =" , 2 ** 3)
+print("modulus (6 % 4 ) =" , 6 % 4)
+print("exponent (2 ** 3 ) =" , 2 ** 3)
+print("exponent (-2 ** 3 ) =" , -2 ** 3)
