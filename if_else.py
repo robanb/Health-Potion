44dd75ba39106c987e6c27cf5f0434bb71f7ba22
Roba Nath Basnet <robanbth@gmail.com>
2025-03-28 16:06:57 +0600

    if else

---
+++ if_else.py
@@ -0,0 +1,16 @@
+#if condition:
+#  code...
+#elif condition2
+#  code2...
+#else:
+#  code3...
+
+num1 = 200
+num2= 100
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+elif num2> num1:
+    print("num2 is bigger than num1")
+else:
+    print("Both numbers are equal")
