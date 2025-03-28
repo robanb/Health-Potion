0d06029b35f14cb147c4e1a14100c7ab8130087b
Roba Nath Basnet <robanbth@gmail.com>
2025-03-28 15:56:13 +0600

    comparisions

---
+++ comparisions.py
@@ -0,0 +1,15 @@
+A = True
+B = False
+
+print( type(A))
+print( type(B))
+print(A == B)
+
+#  comparision operator: >, <, >=, <=,  ==, !=
+print(1>2)
+print(1>=2)
+print(1<2)
+print(1<=2)
+print(2 == 3)
+print(3 == 3)
+print(2!=3)
